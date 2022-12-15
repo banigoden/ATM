cdcbbdff95a02daae21c42a1a9b5cf8e28f3b202
Denis <denis_bandurin@mail.ru>
2022-12-15 13:32:58 +0100

    Changed data in properties, add singelton to PropertiesFileServise.java

--- src/main/java/com/banigoden/atm/service/PropertiesFileServise.java
+++ src/main/java/com/banigoden/atm/service/PropertiesFileServise.java
@@ -9,10 +9,15 @@ import org.springframework.core.env.Environment;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 
 @Configuration
 public class PropertiesFileServise implements PropertiesService {
    private static PropertiesFileServise singeltonInstance;
+   private Properties properties;
    private ApplicationProperties applicationProperties;
 
 
@@ -35,7 +40,22 @@ public class PropertiesFileServise implements PropertiesService {
 
     @Override
     public ApplicationProperties load(String pathTo) {
+        properties = new Properties();
+        try {
+            InputStream inputStream = this.getClass().getClassLoader().getResourceAsStream(pathTo);
+            properties.load(inputStream);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         applicationProperties = new ApplicationProperties();
         return applicationProperties;
     }
+
+   private void setApplicationProperties(){
+
+   }
+
+   public ApplicationProperties getApplicationProperties(){
+       return applicationProperties;
+   }
 }
